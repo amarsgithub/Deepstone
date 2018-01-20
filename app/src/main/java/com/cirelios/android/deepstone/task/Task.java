16edb0853335a799e21bd1af0c06f89015b5e681
SimonFlash <mcsimonflash@gmail.com>
2018-01-20 07:11:12 -0500

    TaskArrayAdapter

---
+++ app/src/main/java/com/cirelios/android/deepstone/task/Task.java
@@ -0,0 +1,13 @@
+package com.cirelios.android.deepstone.task;
+
+import com.cirelios.android.deepstone.R;
+
+public class Task {
+
+    public int color = 0xFFFFFF;
+    public int experience = 0;
+    public int icon = R.drawable.ic_menu_manage;
+    public String name = "Task";
+    public String description = "Some Task";
+
+}
