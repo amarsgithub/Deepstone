d20dffaadb582c40f1caa8fbca82cefc1c763b0f
SimonFlash <mcsimonflash@gmail.com>
2018-01-20 07:07:51 -0500

    Base task layout

--- app/src/main/java/com/cirelios/android/deepstone/FirstFragment.java
+++ app/src/main/java/com/cirelios/android/deepstone/FirstFragment.java
@@ -12,6 +12,12 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.cirelios.android.deepstone.task.Task;
+import com.cirelios.android.deepstone.task.TaskArrayAdapter;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * Created by amars on 1/20/2018.
  */
@@ -30,21 +36,35 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.first_layout, container, false);
 
-        String[] tasks = {"Physics HW", "SwampHacks 2018", "Overdue"};
-        ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tasks);
-        ListView listView = (ListView) myView.findViewById(R.id.amarsListView);
-        listView.setAdapter(listAdapter);
+        Task a = new Task();
+        a.name = "Task A";
+        a.color = R.color.metallic_silver;
+        a.experience = 117;
+        Task b = new Task();
+        b.name = "B Task";
+        b.color = R.color.iridium;
+        b.experience = -14;
 
+        System.out.println("TaskArrayAdapter");
+        ListAdapter listAdapter = new TaskArrayAdapter(getActivity(), Lists.newArrayList(a, b));
+        System.out.println("Success!");
+        ListView listView = myView.findViewById(R.id.tasks_list);
+
+        System.out.println("A");
+        listView.setAdapter(listAdapter);
+        System.out.println("B");
         listView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        System.out.println("C");
                         String task = String.valueOf(adapterView.getItemAtPosition(i));
                         Toast.makeText(getActivity(), task, Toast.LENGTH_SHORT).show();
                     }
                 }
 
         );
+        System.out.println("C");
 
         return myView;
 
